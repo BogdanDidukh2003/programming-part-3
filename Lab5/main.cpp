d293eb9b409cf19144ddcf4127b63c3d9560f924
Denys Shutka <shine.at.dusk@gmail.com>
2019-11-21 23:33:34 +0200

    added algorithm

--- Lab5/main.cpp
+++ Lab5/main.cpp
@@ -1,19 +1,23 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <vector>
+#include <set>
 #include <boost/multiprecision/cpp_int.hpp>
 
 #define ONE '1'
-#define ZERO '0'
-
 #define OUTPUT_FILEPATH "../data.out"
 
 using namespace std;
 using namespace boost::multiprecision;
 
-uint256_t convertBinaryToUnsignedInt(const string &binary);
+inline uint256_t convertBinaryToUnsignedInt(const string &binary);
 
-bool isNumberCompatibleForBase(uint256_t number, const uint64_t& base);
+inline bool isNumberCompatibleForBase(uint256_t number, const uint64_t &base);
+
+inline bool isBinaryCompatibleForBase(const string &binary, const uint64_t &base);
+
+inline unsigned int solution(const string &strNumber, const unsigned int &baseNumber);
 
 int main() {
     const vector<string> INPUT_FILEPATH = {"../data1.in", "../data2.in", "../data3.in", "../data4.in"};
@@ -32,11 +36,10 @@ int main() {
     inputFile >> strNumber;
     inputFile >> baseNumber;
 
-    // -----------------------------------------------------------------------------------------------------------------
-
-    // -------------- Algorithm --------------
-
-    // -----------------------------------------------------------------------------------------------------------------
+    minParts = int(solution(strNumber, baseNumber));
+    if (minParts >= strNumber.size()) {
+        minParts = -1;
+    }
 
     ofstream outputFile;
     outputFile.open(OUTPUT_FILEPATH, ios::out);
@@ -46,7 +49,7 @@ int main() {
     return 0;
 }
 
-uint256_t convertBinaryToUnsignedInt(const string &binary) {
+inline uint256_t convertBinaryToUnsignedInt(const string &binary) {
     uint256_t decimal = 0;
     int size = binary.size();
     for (int i = size - 1; i >= 0; i--) {
@@ -57,7 +60,7 @@ uint256_t convertBinaryToUnsignedInt(const string &binary) {
     return decimal;
 }
 
-bool isNumberCompatibleForBase(uint256_t number, const uint64_t& base) {
+inline bool isNumberCompatibleForBase(uint256_t number, const uint64_t &base) {
     if (number == 1) {
         return true;
     }
@@ -71,4 +74,30 @@ bool isNumberCompatibleForBase(uint256_t number, const uint64_t& base) {
     } while (number > 1 && !remainder);
 
     return (number == 1 && !remainder);
+}
+
+inline bool isBinaryCompatibleForBase(const string &binary, const uint64_t &base) {
+    return isNumberCompatibleForBase(convertBinaryToUnsignedInt(binary), base);
+}
+
+inline unsigned int solution(const string &strNumber, const unsigned int &baseNumber) {
+    if (isBinaryCompatibleForBase(strNumber, baseNumber)) {
+        return 1;
+    }
+    unsigned int currentSolution = strNumber.size();
+    set<unsigned int> allSolutions;
+
+    for (unsigned long k = 1; k < strNumber.size(); k++) {
+        if (isBinaryCompatibleForBase(strNumber.substr(k, strNumber.size() - 1), baseNumber)) {
+            allSolutions.insert(solution(strNumber.substr(0, k), baseNumber) + 1);
+        }
+    }
+
+    for (const auto &sol : allSolutions) {
+        if (sol < currentSolution) {
+            currentSolution = sol;
+        }
+    }
+
+    return currentSolution;
 }
